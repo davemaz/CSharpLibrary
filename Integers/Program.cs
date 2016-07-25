1c9698d4360328a8ac51a4687ada77af73b99dc6
David Mazmanian <davidmazmanian@hotmail.com>
2016-07-25 10:40:38 -0400

    First commit

---
+++ Integers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 49;
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
