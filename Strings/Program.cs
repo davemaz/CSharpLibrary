bb1cad14f388fc8a327f04812a9a89d0b3c9f8bc
David Mazmanian <davidmazmanian@hotmail.com>
2016-07-26 10:28:38 -0400

    Added strings

---
+++ Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(StringUtils.Reverse(message));
+
+            Console.ReadLine();
+        }
+
+    }
+
+    public static class StringUtils
+    {
+        public static string Reverse(string input)
+        {
+            char[] text = input.ToCharArray();
+            Array.Reverse(text);
+            return new string(text);
+        }
+    }
+}
