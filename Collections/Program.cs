785fd6c78f5a467132491b8fbd54c35cdeb643c8
David Mazmanian <David Mazmanian>
2016-07-28 21:01:38 -0400

    Updated interfaces

---
+++ Collections/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<char> s = new Stack<char>();
+            s.Push('A');
+            s.Push('B');
+            s.Push('C');
+
+            Console.WriteLine("Current Stack:");
+            foreach(char c in s)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine($"Next in stack: {s.Peek()}");
+
+            s.Push('D');
+
+            Console.WriteLine($"Next in stack: {s.Peek()}");
+
+            char letter = s.Pop();
+            Console.WriteLine($"Popped: {letter}");
+            s.Pop();
+
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in s)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine();
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+
+            Console.WriteLine("Current Queue:");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Next in queue: {q.Peek()}");
+
+            q.Enqueue(4);
+
+            Console.WriteLine($"Next in queue: {q.Peek()}");
+
+            var num = q.Dequeue();
+            Console.WriteLine($"Dequeued: {num}");
+            q.Dequeue();
+
+            Console.WriteLine("Current Queue:");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,6 +10,79 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            Student beavis = new Student("Beavis");
+            Student butthead = new Student("Butt-head");
+            Student daria = new Student("Daria");
+
+            beavis.Gpa = 2.00f;
+            butthead.Gpa = 2.01f;
+            daria.Gpa = 4.00f;
+
+            List<Student> students = new List<Student>();
+            students.Add(beavis);
+            students.Add(butthead);
+            students.Add(daria);
+
+            Console.WriteLine("Order Added To List:");
+            foreach (Student s in students)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            students.Sort();
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted by GPA (High to Low):");
+            foreach (Student s in students)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson
+    {
+        string Name { get; set; }
+
+        string IntroduceYourself();
+    }
+
+    class Student : IPerson, IComparable<Student>
+    {
+        public string Name { get; set; }
+
+        public float Gpa { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+            Gpa = 0.0f;
+        }
+
+        public string IntroduceYourself()
+        {
+            return $"Hi, my name is {Name} and my GPA is {Gpa:f}.";
+        }
+
+        public override string ToString()
+        {
+            string introduction = IntroduceYourself();
+            return introduction;
+        }
+
+        public int CompareTo(Student other)
+        {
+            if(other is Student)
+            {
+                var student = (Student)other;
+                return student.Gpa.CompareTo(this.Gpa);
+            }
+            else
+            {
+                throw new ArgumentException("Not a student.");
+            }
         }
     }
 }
